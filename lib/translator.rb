123cf2b0074d2580d07e68c5c846395e2be0a69d
Cindy Cai <caicindy87@gmail.com>
2020-03-07 21:16:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -33,6 +33,7 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   new_hash = load_library(file_path)
+  binding.pry
   new_hash["get_emoticon"].each do |key, value|
     if emoticon != key
       return "Sorry, that emoticon was not found"
