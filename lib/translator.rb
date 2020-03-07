a93e896bfef7fa533e5725dd51ba2ae34e72858b
Cindy Cai <caicindy87@gmail.com>
2020-03-07 20:27:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_japanese_emoticon(file_path, emoticon)
     emoticons_hash.each do |meaning, emoticons_array|
       eng, jap = emoticons_array 
         if eng == emoticon
-          returns load_library(file_path)['get_emoticon'][emoticon]
+          return load_library(file_path)['get_emoticon'][emoticon]
         else
           "Sorry, that emoticon was not found"
         end
